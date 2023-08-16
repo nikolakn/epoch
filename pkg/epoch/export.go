ced79d61ae69731b0633d0b5cb18c212fdc4a3c2
nikolakn <nkcodeplus@gmail.com>
2023-08-16 21:00:43 +0200

    update

--- pkg/epoch/export.go
+++ pkg/epoch/export.go
@@ -1,6 +1,8 @@
 package epoch
 
 import (
+	jd "epoch/internal/julian"
+	"fmt"
 	"log"
 	"os"
 )
@@ -14,20 +16,52 @@ func (doc *Document) ExportHtml(file string) {
 	body += `
 	<table>
 	<tr>
-	  <th>Company</th>
-	  <th>Contact</th>
-	  <th>Country</th>
+	  <th>Start</th>
+	  <th>End</th>
+	  <th>Duration</th>
+	  <th>Name</th>
+	  <th>Description</th>
+	  <th>Url</th>
 	</tr>
-	<tr>
-    <td>Alfreds Futterkiste</td>
-    <td>Maria Anders</td>
-    <td>Germany</td>
-	</tr>
-	<tr>
-		<td>Centro comercial Moctezuma</td>
-		<td>Francisco Chang</td>
-		<td>Mexico</td>
-	</tr>
+	`
+	for _, e := range doc.Events {
+		start := jd.JDToTime(e.GetStart())
+		end := jd.JDToTime(e.GetDuration())
+		duration := (e.GetDuration() - e.GetStart()) / JDYear
+		iy := int(duration)
+		y_ostatak := 12.0 * float64(duration-float64(iy))
+		d2 := fmt.Sprintf("%.2f years ~(%d years and %0.2f months)\n", duration, iy, y_ostatak)
+
+		if e.Relative() || e.EndRelative() {
+			duration = e.GetDuration() / JDYear
+			iy := int(duration)
+			y_ostatak := 12.0 * float64(duration-float64(iy))
+			d2 = fmt.Sprintf("%f years ~(%d years and %0.2f months)\n", duration, iy, y_ostatak)
+			end = jd.JDToTime(e.GetStart() + e.GetDuration())
+		}
+
+		body += "<tr>\n"
+		body += "<td>" + fmt.Sprintf(" %d.%d.%d", start.Day(), start.Month(), start.Year()) + "</td>\n"
+		if e.GetDuration() == 0 {
+			body += "<td></td>\n"
+		} else {
+			body += "<td>" + fmt.Sprintf(" %d.%d.%d", end.Day(), end.Month(), end.Year()) + "</td>\n"
+		}
+		if e.GetDuration() == 0 {
+			body += "<td></td>\n"
+
+		} else {
+			body += "<td>" + d2 + "</td>\n"
+		}
+		body += "<td>" + fmt.Sprintf("%s", e.GetEpoch().Title) + "</td>\n"
+		body += "<td>" + fmt.Sprintf("%s", e.GetEpoch().Description) + "</td>\n"
+		body += "<td>" + fmt.Sprintf("%s", e.GetEpoch().Url) + "</td>\n"
+
+		body += "</tr>\n"
+
+	}
+
+	body += `
 	</table>
 	`
 	html := `
